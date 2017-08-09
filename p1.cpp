a209185808c32c51bc6d074f07caddc437f6125b
Alexander Giang <alexgiang96@gmail.com>
2017-08-09 14:31:40 -0700

    added while-loop comment

--- p1.cpp
+++ p1.cpp
@@ -12,6 +12,7 @@ int main() {
     int var = 0;
     int sum = 0;
 
+// if a number is a multiple of 3 or 5, add to sum
     while(var < 1000) {
         if( var%3 == 0 || var%5 == 0  ) {
             sum = sum + var;
